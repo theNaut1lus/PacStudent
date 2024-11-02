b6a434603e02978c695f6b941bd40fe64d36c4fd
Sidney Aulakh <sidneyaulakh@gmail.com>
2024-11-02 20:23:00 +1100

    Game, Audio and Movement Manager setup: start PacStudent movement

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,26 +4,99 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    GameManager gameManager;
+    
+    //game music and start music
     [SerializeField]
     private AudioSource startMusic;
     [SerializeField]
     private AudioSource gameMusic;
-    bool start = true;
+    
+    //music for when pacstudent dies
+    [SerializeField]
+    private AudioSource deadMusic;
+    
+    //music for when ghosts are edible
+    [SerializeField] 
+    private AudioSource scaredMusic;
+    
+    //clip for when pacstudent eats a cherry
+    [SerializeField]
+    private AudioSource cherryClip;
+    public void PlayEatSound() {
+        cherryClip.Play();
+    }
+    
+    //clip for when pacstudent collides with a wall
+    [SerializeField]
+    private AudioSource collideClip;
+    public void PlayCollisionSound() {
+        collideClip.Play();
+    }
+    
+    //clip for when ghost is eaten
+    [SerializeField]
+    private AudioSource dieClip;
+    public void PlayDieSound() {
+        dieClip.Play();
+    }
+    
+    //clip for when pacstudent moves
+    [SerializeField]
+    private AudioSource moveClip;
+    public void PlayMoveSound() {
+        moveClip.Play();
+    }
+    
+    //this keeps track of current music playing
+    private AudioSource currentMusic;
+    
+    
+    //initialise the AudioManager
+    public void Init(GameManager gameManager)
+    {
+        this.gameManager = gameManager;
+        currentMusic = startMusic;
+    }
+    
     // Start is called before the first frame update
+    
+    //music/clip management now handled by other managers based on type of movement or game situation, audio manager just provides the variables and the corresponding public functions
     void Start()
     {
-        Debug.Log(startMusic.clip.length.ToString());
-        startMusic.Play();
         
     }
-
     // Update is called once per frame
     void Update()
     {
-        if (Time.time > startMusic.clip.length && start) {
-            startMusic.Stop();
-            gameMusic.Play();
-            start = false;
-        }
+
+    }
+
+    //switch to start music
+    public void PlayStartMusic() {
+        currentMusic.Stop();
+        startMusic.Play();
+        currentMusic = startMusic;
+    }
+
+    //switch to scared music
+    public void PlayScaredMusic() {
+        currentMusic.Stop();
+        scaredMusic.Play();
+        currentMusic = scaredMusic;
+    }
+
+    //switch to game music
+    public void PlayNormalMusic() {
+        currentMusic.Stop();
+        gameMusic.Play();
+        currentMusic = gameMusic;
+    }
+    
+    //switch to dead music
+    public void PlayDeadMusic() {
+        currentMusic.Stop();
+        deadMusic.Play();
+        currentMusic = deadMusic;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    //define all managers and generators
+    public MovementManager movementManager;
+    public AudioManager audioManager;
+    public UIManager uiManager;
+    public LevelGenerator levelGenerator;
+    
+    //variables to keep track of the current lives and score
+    private int score = 0;
+    public int lives = 3;
+    
+    //variables for whether game is paused or in startgame stage
+    public bool isPaused = false;
+    public bool isGameStarted = true;
+
+    private void Awake()
+    {
+        //get all the managers and init them
+        movementManager.Init(this);
+        
+        //[TODO]: create respective init functions for the other managers and levelgenerator
+        //audioManager.Init(this);
+        //uiManager.Init(this);
+        //levelGenerator.Init(this);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +41,64 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+    
+    IEnumerator StartGame() {
+        lives = 3;
+        //uIManager.Reset();
+        movementManager.pacStudentController.MoveToStart();
+        //audioManager.PlayStartMusic();
+        yield return null;
+        //audioManager.PlayNormalMusic();
+        //uIManager.StartTimer();
+        Play();
+    }
+    
+    public void AddScore(int x) {
+        score += x;
+        //uIManager.SetScore(score);
+    }
+    
+    public void Play() {
+        isGameStarted = true;
+        isPaused = false;
+        //movementManager.cherryController.StartCherrySpawn();
+    }
+    
+    public void Pause() {
+        isPaused = true;
+        //movementManager.cherryController.StopCherrySpawn();
+    }
+    
+    public void GameOver() {
+        Pause();
+        movementManager.pacStudentController.pacStudentAnimator.enabled = false;
+        SaveHighScore();
+        //uIManager.ShowGameOver();
+        Invoke("ShowStartScene", 3);
+    }
+    
+    public void SaveHighScore() {
+        int currentHighScore = PlayerPrefs.GetInt("highscore", 0);
+        float currentTime = PlayerPrefs.GetFloat("highscoreTime", 0.0f);
+
+        //[TODO]: Save game logic
+        /*if(score > currentHighScore) {
+            PlayerPrefs.SetInt("highscore", score);
+            //PlayerPrefs.SetFloat("highscoreTime", uIManager.GetTimePlayed());
+        } else if(score == currentHighScore) {
+            if(currentTime > uIManager.GetTimePlayed()) {
+                PlayerPrefs.SetInt("highscore", score);
+                //PlayerPrefs.SetFloat("highscoreTime", uIManager.GetTimePlayed());
+            }
+        }*/
+    }
+
+    public void ShowStartScene() {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ShowLevel1() {
+        SceneManager.LoadScene(1);
     }
 }
